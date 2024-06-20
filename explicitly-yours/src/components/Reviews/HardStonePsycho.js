ddd7d6c12bbea4a88f708154ceb14d420bb2909b
Mateen Gilani <mateengilani@gmail.com>
2024-06-20 17:08:14 -0400

    Update HardStonePsycho.js

--- explicitly-yours/src/components/Reviews/HardStonePsycho.js
+++ explicitly-yours/src/components/Reviews/HardStonePsycho.js
@@ -5,7 +5,7 @@ const HardstonePsycho = () => {
   return (
     <ReviewTemplate
       albumName="Hardstone Psycho" // Ensure this matches the album name on Spotify
-      coverImage="/path-to-album-cover.jpg" // Path to the album cover image
+      coverImage="/don.webp" // Path to the album cover image
       artistName="Don Toliver"
       synopsis="Cementing his position in rap's top tier, Don Toliver's 'Hardstone Psycho' blends core elements with fresh aspects, featuring collaborations with Kodak Black and Future. Despite some jarring rock influences, the album's highlights, like 'Deep In The Water' and 'Attitude', showcase his evolving prowess in hip-hop."
       rating={80}
