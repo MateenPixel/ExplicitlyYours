3f98ded99a862c571301c9c085a654b8ce9fdb1e
Mateen Gilani <mateengilani@gmail.com>
2024-06-21 14:36:47 -0400

    fixed for mobile

--- explicitly-yours/src/components/Header/AboutMe.js
+++ explicitly-yours/src/components/Header/AboutMe.js
@@ -6,13 +6,11 @@ const AboutMe = () => {
   return (
     <div className="resume-container">
       <h1 className="resume-title">Mateen Gilani</h1>
-      <h2 className="resume-subtitle">Professional Music Listener</h2>
+      <h2 className="resume-subtitle">Professional Hater</h2>
       <div className="resume-section">
         <h3 className="resume-section-title">Summary</h3>
         <p className="resume-text">
-          Enthusiastic music aficionado with over 1000 hours of annual listening experience.
-           Skilled in recognizing beats, analyzing lyrics, and calling Drake cringe. 
-           Committed to staying updated with the latest in the music world and sharing insights with fellow enthusiasts.
+          Music good. Soul Train bad.
         </p>
       </div>
       <div className="resume-section">
@@ -22,15 +20,15 @@ const AboutMe = () => {
             <strong>Chief Listening Officer</strong> at <em>Home</em> (2018 - Present)
             <p className="resume-text">
               - Listened to various genres of music for over 2000 hours.
-              <br />- Curated monthly playlists for my ex and whoever else cared.
-              <br />- Reviewed and rated 1 album so far.
+              <br />- Curated monthly playlists to share with the masses (3 people).
+              <br />- Reviewed and rated 1 album.
             </p>
           </li>
           <li>
             <strong>MF Thug</strong> (2003 - 2018)
             <p className="resume-text">
               - Put people on.
-              <br />- Changed opinion on Drake being better than Kanye (my fault).
+              <br />- Realized Kanye is better than Drake.
               <br />- Knew the lyrics to T-Wayne Nasty Freestyle.
             </p>
           </li>
@@ -40,7 +38,7 @@ const AboutMe = () => {
         <h3 className="resume-section-title">Skills</h3>
         <ul className="resume-list">
           <li>Listening to the same 5 songs on repeat for countless days.</li>
-          <li>Recognizing songs by the drum/bass.</li>
+          <li>Recognizing songs by the bass when I'm at the pizza place underneath the bar.</li>
           <li>Creating playlists with/for others to enjoy (venmo me 5).</li>
           <li>Writing engaging music reviews!</li>
         </ul>
@@ -48,17 +46,19 @@ const AboutMe = () => {
       <div className="resume-section">
         <h3 className="resume-section-title">Education</h3>
         <p className="resume-text">
-          B.A. in Making Terrible Beats through GarageBand (2010 - 2014)
-        <p className='resume-text'></p>
           B.A. in Computer Science (2021 - 2025)
+        <p className='resume-text'></p>
+          PhD in Jugding Others Music Preferences (Accelerated) (2016-2021)
+        <p className='resume-text'></p>
+          B.A. in Making Terrible Beats through GarageBand (2012 - 2016)
         </p>
       </div>
       <div className="resume-section">
         <h3 className="resume-section-title">Hobbies</h3>
         <p className="resume-text">
-          - Driving perhaps a little too fast with the music blaring, windows down through every neighborhood
+          - Driving fast, music loud in old folks neighborhoods.
           <br />- Convincing people that questionable music is good music
-          <br />- ROCK PAPER STRIPPERS
+          <br />- Separating art from the artist!
         </p>
       </div>
       <Link to="/" className="back-link">← Back to Home</Link>
